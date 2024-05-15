543f477ff21db46339349455f21ad4475fe66af2
Orive2023 <orivesolutions8@gmail.com>
2024-05-15 18:32:00 +0530

    login page ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { useState } from 'react';
+import Login from "./components/Login"
+import "./style.css"
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <Router>
+        <Routes>
+        <Route path='/' element={<Login/>}/>
+        {/* <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
